e4f6244664096016ff07ea72bba675c6d88a2fdb
irishcarbomb777 <danielclannan@gmail.com>
2024-03-07 12:57:18 -0500

    mod

--- src/clients/dynamodb.ts
+++ src/clients/dynamodb.ts
@@ -2,7 +2,6 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 
-
 export const dynamodb = DynamoDBDocument.from(
     new DynamoDBClient({}),
 );
