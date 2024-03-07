51fa57761c1050a02b3116dbe91533d380ebde27
irishcarbomb777 <danielclannan@gmail.com>
2024-03-07 14:22:10 -0500

    mod

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 
 
 export {dynamodb} from "./clients/dynamodb";
+export {
+    ScanCommandOutput,
+    QueryCommandOutput,
+    GetCommandOutput,
+    PutCommandOutput,
+    UpdateCommandOutput,
+    DeleteCommandOutput,
+} from "./types/CommandOutputs";
