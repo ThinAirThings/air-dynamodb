373cea49d471eb8d6dbdc56b90315fceb3dfc1dd
irishcarbomb777 <danielclannan@gmail.com>
2024-03-07 13:45:41 -0500

    mod

--- src/clients/dynamodb.ts
+++ src/clients/dynamodb.ts
@@ -1,6 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import { QueryCommandOutput } from "../types/CommandOutputs";
+import { DynamoDBDocument  } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommandOutput, GetCommandOutput, PutCommandOutput, QueryCommandOutput, ScanCommandOutput, UpdateCommandOutput } from "../types/CommandOutputs";
 
 
 const _dynamodb = DynamoDBDocument.from(
@@ -8,17 +8,17 @@ const _dynamodb = DynamoDBDocument.from(
 );
 
 export const dynamodb = {
-    scan: async <T extends Record<string, any>>(input: Parameters<typeof _dynamodb['scan']>[0]): Promise<QueryCommandOutput<T>> => 
-        _dynamodb.scan(input) as Promise<QueryCommandOutput<T>>,
+    scan: async <T extends Record<string, any>>(input: Parameters<typeof _dynamodb['scan']>[0]): Promise<ScanCommandOutput<T>> => 
+        _dynamodb.scan(input) as Promise<ScanCommandOutput<T>>,
     query: async <T extends Record<string, any>>(input: Parameters<typeof _dynamodb['query']>[0]): Promise<QueryCommandOutput<T>> => 
         _dynamodb.query(input) as Promise<QueryCommandOutput<T>>,
-    get: async <T extends Record<string, any>>(input: Parameters<typeof _dynamodb['get']>[0]): Promise<QueryCommandOutput<T>> =>
-        _dynamodb.get(input) as Promise<QueryCommandOutput<T>>,
-    put: async (input: Parameters<typeof _dynamodb['put']>[0]): Promise<QueryCommandOutput<Record<string, any>>> =>
-        _dynamodb.put(input) as Promise<QueryCommandOutput<Record<string, any>>>,
-    update: async (input: Parameters<typeof _dynamodb['update']>[0]): Promise<QueryCommandOutput<Record<string, any>>> =>
-        _dynamodb.update(input) as Promise<QueryCommandOutput<Record<string, any>>>,
-    delete: async (input: Parameters<typeof _dynamodb['delete']>[0]): Promise<QueryCommandOutput<Record<string, any>>> =>
-        _dynamodb.delete(input) as Promise<QueryCommandOutput<Record<string, any>>>,
+    get: async <T extends Record<string, any>>(input: Parameters<typeof _dynamodb['get']>[0]): Promise<GetCommandOutput<T>> =>
+        _dynamodb.get(input) as Promise<GetCommandOutput<T>>,
+    put: async (input: Parameters<typeof _dynamodb['put']>[0]): Promise<PutCommandOutput> =>
+        _dynamodb.put(input) as Promise<PutCommandOutput>,
+    update: async (input: Parameters<typeof _dynamodb['update']>[0]): Promise<UpdateCommandOutput> =>
+        _dynamodb.update(input) as Promise<UpdateCommandOutput>,
+    delete: async (input: Parameters<typeof _dynamodb['delete']>[0]): Promise<DeleteCommandOutput> =>
+        _dynamodb.delete(input) as Promise<DeleteCommandOutput>,
 }
 
